2911cdc954e73158cf897220b48fe4b543e730a5
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-25 00:21:08 -0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Christian Manzaraz on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
